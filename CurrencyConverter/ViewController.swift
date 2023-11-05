573f392ec81fa721c74b21c663e75f1099a9357d
İsmail Harmanda <iharmanda@hotmail.com>
2023-11-05 23:12:20 +0300

    create URL, url session and api tas

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -28,6 +28,24 @@ class ViewController: UIViewController {
     
     
     @IBAction func onPressGetRates(_ sender: UIButton) {
+        
+        let url = URL(string: "http://data.fixer.io/api/latest?access_key=2addceb4937e131ddc7047aa2cf1204c")
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: URLRequest(url: url!)) { data, response, error in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                let okButton = UIAlertAction(title: "OK", style: .default, handler:nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                
+                if data != nil {
+                    
+                }
+            }
+        }
     }
     
 }
